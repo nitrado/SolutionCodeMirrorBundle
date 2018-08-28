aaee7ed5b0e14a3c3401fd8aad95d5979b83c1b5
Tommy Quissens <tommy.quissens@storefront.be>
2018-08-28 18:11:36 +0200

    updated options handling
    same

--- Form/Type/CodeMirrorType.php
+++ Form/Type/CodeMirrorType.php
@@ -5,7 +5,7 @@ namespace Solution\CodeMirrorBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CodeMirrorType extends AbstractType
 {
@@ -30,7 +30,7 @@ class CodeMirrorType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
        $resolver->setDefaults(
            array(
